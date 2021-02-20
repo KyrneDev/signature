588719ae6de249dcad7451da8d6e037331a169c9
KyrneDev <charlie.kern01@gmail.com>
2021-02-20 09:58:27 -0800

    beta 15

--- extend.php
+++ extend.php
@@ -1,37 +1,44 @@
 <?php
-use Flarum\Forum\Content\AssertRegistered;
+
 use Illuminate\Contracts\Events\Dispatcher;
 use XEngine\Signature\Validation\ValidateSignature;
 use XEngine\Signature\Model;
 use Flarum\Extend;
+use Flarum\User\User;
+use Flarum\Api\Serializer\UserSerializer;
 use Flarum\Frontend\Document;
 use FoF\Components\Extend\AddFofComponents;
 
 return [
-  new AddFofComponents(),
+    new AddFofComponents(),
 
-  (new Extend\Frontend('forum'))
-  ->content(function (Document $document) {
-      $document->head[] = '<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script>window.jQuery || document.write(\'<script src="js/vendor/jquery-3.3.1.min.js"><\/script>\')</script><script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.4.0/trumbowyg.js"></script>';
-  }),
+    (new Extend\Frontend('forum'))
+        ->content(function (Document $document) {
+            $document->head[] = '<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script>window.jQuery || document.write(\'<script src="js/vendor/jquery-3.3.1.min.js"><\/script>\')</script><script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.4.0/trumbowyg.js"></script>';
+        }),
 
-new Extend\Locales(__DIR__.'/locale'),
-        
-(new Extend\Frontend('forum'))
-  ->js(__DIR__.'/js/dist/forum.js')
-  ->css(__DIR__.'/less/signature.less')
-  ->css(__DIR__.'/less/trumbowyg.less'),
-  
-(new Extend\Frontend('admin'))
-  ->js(__DIR__.'/js/dist/admin.js'),
-  
-(new Extend\Frontend('forum'))
-  ->route('/settings/signature', 'settings.signature'),
-  
-(new Extend\Routes('api'))
-  ->post('/settings/signature/validate', 'settings.signature', ValidateSignature::class),
+    new Extend\Locales(__DIR__ . '/locale'),
 
-function (Dispatcher $events) {
-  $events->subscribe(Model\UserSignatureAttributes::class);
-}
+    (new Flarum\Extend\ApiSerializer(UserSerializer::class))
+        ->mutate(function (UserSerializer $serializer, User $user, array $attributes) {
+            $attributes['signature'] = $user->signature;
+
+            return $attributes;
+        }),
+
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/less/signature.less')
+        ->css(__DIR__ . '/less/trumbowyg.less')
+        ->route('/settings/signature', 'settings.signature', \Flarum\Forum\Content\AssertRegistered::class),
+
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__ . '/js/dist/admin.js'),
+
+    (new Extend\Routes('api'))
+        ->post('/settings/signature/validate', 'settings.signature', ValidateSignature::class),
+
+    function (Dispatcher $events) {
+        $events->subscribe(Model\UserSignatureAttributes::class);
+    }
 ];
